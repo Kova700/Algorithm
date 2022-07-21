7f28dcc6515adcb6c87ef284376037151ebdc51c
Tom <blak1002@gmail.com>
2022-07-21 12:43:26 +0900

    BOJ1759

--- src/main/kotlin/DAY01/P1759/Main.kt
+++ src/main/kotlin/DAY01/P1759/Main.kt
@@ -1,45 +1,65 @@
 package DAY01.P1759
 
-val answer = ArrayList<String>()
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
+
+lateinit var isVisited :MutableList<Boolean>
+lateinit var answer :ArrayList<String>
+lateinit var givenChars :List<Char>
 fun main(){
-//    val (L,C) = readLine()!!.split(' ').map{it.toInt()}
-//    val givenChars = readLine()!!.split(' ').map { it[0] }.sorted()
-//    val visited = MutableList(C){false}
-//
-//    dfs(L,visited, listOf(),givenChars,0)
-//
-//    answer.forEach{
-//        println(it)
-//    }
+    System.setIn(FileInputStream("src/main/kotlin/DAY01/P1759/input"))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val (L,C) = br.readLine()!!.split(' ').map{it.toInt()}
+    givenChars = br.readLine()!!.split(' ').map { it[0] }.sorted()
+    isVisited = MutableList(C){false}
+    answer = ArrayList()
+
+    dfs(L,0)
+
+    answer.forEach{
+        println(it)
+    }
 
 }
-//fun dfs(L :Int,
-//        visited : MutableList<Boolean>,
-//        store : List<Char>,
-//        givenChars : List<Char>,
-//        start :Int ){
-//    for (i in start until givenChars.size ){
-//        //1.체크인 : visited[] = true
-//        visited[i] = true
-//        //2.목적지인가? : 글자 수 체크 & 자음 모음의 개수
-//        if(store.size == L){
-//            if(checkStore(store)){
-//                answer.add(store.joinToString(""))
-//                return
-//            }
-//        }else{
-//            dfs(L,visited,store + ,givenChars,i+1)
-//            //3.연결된 곳을 순회 : 0 ~ c Or index ~ c
-//            //   4.갈 수 있는가? : visited[]
-//            //      5.간다
-//        }
-//        //6.체크아웃 : visited[] = false
-//        visited[i] = false
-//    }
-//}
-//fun checkStore(list : List<Char>) : Boolean{
-//    //최소 한 개의 모음
-//    //최소 두 개의 자음을 가지고 있어야함
-//
-//    return false
-//}
+fun dfs(L :Int,
+        index :Int ,
+        count :Int = 0){
+
+    //2.목적지인가? : L개의 알파벳을 뽑았는가?
+    if(count == L){
+        val temp = givenChars.filterIndexed{index, _ ->  isVisited[index]}
+        // 최소 한 개의 모음 && 최소 두 개의 자음을 갖추었는가?
+        if(checkStore(temp,L)){
+            //암호를 이루는 알파벳이 암호에서 증가하는 순서로 배열되었을 거다.(사전에 이미 정렬함)
+            answer.add(temp.joinToString(""))
+            return
+        }
+    }
+
+    //3.연결된 곳을 순회 : 0 ~ c Or index ~ c
+    for (i in index until givenChars.size ){
+        // 4.갈 수 있는가? : visited[]
+        if (isVisited[i] == false){
+            // 5.간다
+            //1.체크인 : visited[] = true
+            isVisited[i] = true
+            dfs(L,i+1,count+1)
+            //6.체크아웃 : visited[] = false
+            isVisited[i] = false
+        }
+
+    }
+}
+fun checkStore(list : List<Char>, L :Int) : Boolean{
+    val moList = listOf('a','e','i','o','u')
+    var count = 0
+    for(mo in moList){
+        if(list.contains(mo)) count++
+    }
+    //최소 한 개의 모음 , 최소 두 개의 자음을 가지고 있어야함
+    if( L-2 < count || count < 1) return false
+
+    return true
+}
