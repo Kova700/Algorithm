5aa76b95518e5568ac7350a0c6f39a68ddf43f5c
Tom <blak1002@gmail.com>
2022-07-21 12:37:20 +0900

    BOJ1062 structure refector

--- src/main/kotlin/DAY01/P1062/Main.kt
+++ src/main/kotlin/DAY01/P1062/Main.kt
@@ -3,14 +3,14 @@ import java.io.FileInputStream
 import java.io.InputStreamReader
 
 lateinit var isVisited :MutableList<Boolean>
+lateinit var givenWords :Array<String>
 var maxCount = 0
 fun main(){
     System.setIn(FileInputStream("src/main/kotlin/DAY01/P1062/input.txt"))
-
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (N,K) = br.readLine().split(' ').map{it.toInt()}
     //val givenWords = Array(N){br.readLine().toString().replace("[antic]".toRegex(),"")}
-    val givenWords = Array(N){br.readLine().toString()}
+    givenWords = Array(N){br.readLine().toString()}
 
     // anta, tica 에서 배울 수 있는 글자 5개
     if (K < 5) {
@@ -33,17 +33,18 @@ fun main(){
 //    val allAlphabets = ('a'..'z').toList()
 
     //K개의 글자를 가르칠 때 , 학생들이 읽을 수 있는 단어 개수의 최댓값을 출력
-    dfs(K,givenWords,0)
+    dfs(K,0,5)
+
     println(maxCount)
 }
-//재귀시에 문제 메모리 커트라인이 낮다면 파라미터에 메모리를 많이 잡아먹는 요소를 줄이자.
+//재귀시에 메모리 소비가 많다면 파라미터에 메모리를 많이 잡아먹는 요소를 줄이자.
+//(전역 변수로 변경)
 fun dfs(K :Int ,
-        givenWords :Array<String> ,
         index :Int,
-        cnt :Int = 0){
-//2.목적지인가? : 목적지 = 깊이가 K
-//    -> 읽은 수 있는 단어의 개수 계산 후 Max 갱신
-    if(cnt == K - 5){ //여기 차이 인가?
+        cnt :Int){
+    //2.목적지인가? : 목적지 = 깊이가 K
+    if(cnt == K){
+        // -> 읽은 수 있는 단어의 개수 계산 후 Max 갱신
         var count = 0
         for (word in givenWords){
             var canRead = true
@@ -59,15 +60,15 @@ fun dfs(K :Int ,
         return
     }
     //3.연결된 곳을 순회 : 연결된 곳 = 모든 알파벳
-    for (i in index+1 until  26){
+    for (i in index until  26){
         //4.갈 수 있는가? : 아직 배우지 않은 알파벳
         if(isVisited[i] == false){
             //5.간다
             //1.체크인
             isVisited[i] = true
-            dfs(K,givenWords,i,cnt+1)
-            isVisited[i] = false
+            dfs(K,i+1,cnt+1)
             //6.체크아웃
+            isVisited[i] = false
         }
     }
 }
